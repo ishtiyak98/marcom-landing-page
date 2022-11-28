624cac8cef4f561c877ced687f322dd8b5e3099b
ishtiyak98 <rifatishtiyak@gmail.com>
2022-11-28 13:48:26 +0600

    responsive style added

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -10,20 +10,17 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const HomeBanner = () => {
   return (
-    <section
-      className="h-[90vh] flex items-center bg-no-repeat bg-cover bg-right-bottom px-24 relative"
-      style={{ backgroundImage: `url(${BgImg})` }}
-    >
+    <section className="h-[calc(100vh-56px)] lg:h-[calc(100vh-66px)] background_img flex items-center bg-no-repeat bg-cover lg:bg-right-bottom px-3 lg:px-24 relative">
       <div className="space-y-8">
         <div>
-          <h4 className="text-3xl uppercase font-semibold text-[#00DBB9] montserrat-font tracking-wide">
+          <h4 className="text-2xl lg:text-3xl uppercase font-semibold text-[#00DBB9] montserrat-font tracking-wide">
             we are the creative
           </h4>
-          <h2 className="text-6xl uppercase font-semibold text-white montserrat-font">
+          <h2 className="text-5xl uppercase font-semibold text-white montserrat-font">
             Marketing <br /> Agency
           </h2>
         </div>
-        <p className="w-[450px] text-white text-lg">
+        <p className="w-[300px] lg:w-[450px] text-white text-lg">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt,
           voluptate rerum? Non tenetur ipsam animi temporibus.
         </p>
@@ -31,16 +28,21 @@ const HomeBanner = () => {
           See More
         </button>
         <div className="flex space-x-6 pt-4">
-          <GiArcheryTarget className="text-6xl text-pink-600 bg-white rounded-full p-3 cursor-pointer"></GiArcheryTarget>
-          <AiOutlinePieChart className="text-6xl text-[#00DBB9] bg-white rounded-full p-3 cursor-pointer"></AiOutlinePieChart>
-          <AiOutlineBulb className="text-6xl text-[#FFB000] bg-white rounded-full p-3 cursor-pointer"></AiOutlineBulb>
+          <GiArcheryTarget className="text-5xl lg:text-6xl text-pink-600 bg-white rounded-full p-3 cursor-pointer"></GiArcheryTarget>
+          <AiOutlinePieChart className="text-5xl lg:text-6xl text-[#00DBB9] bg-white rounded-full p-3 cursor-pointer"></AiOutlinePieChart>
+          <AiOutlineBulb className="text-5xl lg:text-6xl text-[#FFB000] bg-white rounded-full p-3 cursor-pointer"></AiOutlineBulb>
         </div>
       </div>
-      <div className="absolute bottom-8 right-24 space-y-4">
+      <div className="absolute bottom-8 right-3 lg:right-24 space-y-4">
         <FaInstagram className="text-4xl text-[#101928] hover:text-pink-600 bg-[#00DBB9] p-2 rounded-full cursor-pointer"></FaInstagram>
         <FaFacebookF className="text-4xl text-[#101928] hover:text-pink-600 bg-[#00DBB9] p-2 rounded-full cursor-pointer"></FaFacebookF>
         <FaTwitter className="text-4xl text-[#101928] hover:text-pink-600 bg-[#00DBB9] p-2 rounded-full cursor-pointer"></FaTwitter>
         <FaWhatsapp className="text-4xl text-[#101928] hover:text-pink-600 bg-[#00DBB9] p-2 rounded-full cursor-pointer"></FaWhatsapp>
+      </div>
+      <div className="absolute top-16 right-44 hidden lg:block">
+        <p className="text-white text-2xl italic montserrat-font">
+          Supercharge <br /> Your Business <br /> With Us!
+        </p>
       </div>
     </section>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SlOptionsVertical } from "react-icons/sl";
+import { BiMenu } from "react-icons/bi";
 
 import LOGO from "../assets/marcom-agency-logo.png";
 
 const Navbar = () => {
+  const [navState, setNavState] = useState(false);
   return (
-    <nav className="px-14 py-4">
-      <div className="flex justify-between items-center ">
+    <nav className="px-2 lg:px-14 py-2">
+      <div className="flex justify-between items-center relative">
         <div>
-          <img src={LOGO} alt="" width={150} />
+          <img src={LOGO} alt="" className="w-[120px] lg:w-[150px]" />
         </div>
-        <div className="space-x-16 flex items-center">
+        <div className="hidden space-x-16 lg:flex items-center">
           <Link to={"/"} className="text-lg font-medium hover:text-[#00DBB9]">
             Home
           </Link>
@@ -29,7 +31,7 @@ const Navbar = () => {
               type="text"
               className="border-t-2 border-l-2 border-b-2 border-black outline-none w-[210px] px-3 py-1 rounded-l-full text-lg font-medium"
             />
-            <button className="border-t-2 border-r-2 border-b-2 border-black  hover:bg-[#00DBB9] text-lg font-medium px-4 py-1 rounded-r-full">
+            <button className="border-t-2 border-r-2 border-b-2 border-l-2 border-black hover:border-l-2 hover:bg-[#00DBB9] text-lg font-medium px-4 py-1 rounded-r-full">
               search
             </button>
           </div>
@@ -37,7 +39,43 @@ const Navbar = () => {
             <SlOptionsVertical className="text-2xl"></SlOptionsVertical>
           </div>
         </div>
+        <div className="block lg:hidden">
+        
+          <div onClick={() => setNavState(!navState)}>
+            <BiMenu className="text-4xl cursor-pointer"></BiMenu>
+          </div>
+        </div>
       </div>
+      {navState && (
+        <div className="w-full absolute left-0 responsive-nav bg-white z-20 pt-4">
+          <div className="flex flex-col ">
+            <Link
+              to={"/"}
+              className="text-lg font-medium hover:text-white hover:bg-[#00DBB9] py-2 px-2"
+            >
+              Home
+            </Link>
+            <Link
+              to={"/"}
+              className="text-lg font-medium hover:text-white hover:bg-[#00DBB9] py-2 px-2"
+            >
+              About
+            </Link>
+            <Link
+              to={"/"}
+              className="text-lg font-medium hover:text-white hover:bg-[#00DBB9] py-2 px-2"
+            >
+              Workshop
+            </Link>
+            <Link
+              to={"/"}
+              className="text-lg font-medium hover:text-white hover:bg-[#00DBB9] py-2 px-2"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
